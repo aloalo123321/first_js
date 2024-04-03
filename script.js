89ccb92b7727798fbbfd1b816a1d75d5edb7c1ca
aloalo123321 <158489552+aloalo123321@users.noreply.github.com>
2024-04-04 00:25:12 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let your_name = "Vano";
+let age = "16";
+let job = "student";
+let ForThanks = "thanks";
+
+console.log("saxeli:" + " " + your_name);
+console.log("age:" + " " + age);
+console.log("job:" + " " + job);
+console.log("for teacher:" + " " + ForThanks);
